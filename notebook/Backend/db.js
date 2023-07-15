c94894a0e11a2fd8f57b7d2dc0fa955e173bc7a9
kanakSo31 <sunuwalkanak@gmail.com>
2023-07-15 14:45:09 +0530

    Mongo connected @T 3000

--- notebook/Backend/db.js
+++ notebook/Backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const mongoURI = "mongodb://localhost:27017"
 
 const connectToMongo = ()=>{
-    console.log("Connected to Mongoo Successfully")
+    console.log("Mongoo Connected Successfully")
     // mongoose.connect(mongoURI, ()=>{
     // })
 }
