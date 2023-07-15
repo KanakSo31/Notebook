fa82bb0d3781372712ddce05a991abdfbb130343
kanakSo31 <sunuwalkanak@gmail.com>
2023-07-15 14:40:00 +0530

    New Mongo Connection Initiated

--- notebook/Backend/db.js
+++ notebook/Backend/db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const mongoURI = "mongodb://localhost:27017"
 
 const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
-        console.log("Connected to Mongoo Successfully")
-    })
+    console.log("Connected to Mongoo Successfully")
+    // mongoose.connect(mongoURI, ()=>{
+    // })
 }
 module.exports = connectToMongo;
