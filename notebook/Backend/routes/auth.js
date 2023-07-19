538954522e28ba094feb474b7242767239df1e65
kanakSo31 <sunuwalkanak@gmail.com>
2023-07-19 17:11:42 +0530

    backend setup

--- notebook/Backend/routes/auth.js
+++ notebook/Backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/'(req, res)=>{
+router.get('/', (req, res)=>{
     obj = {
         a: 'kanak',
         number: 35
